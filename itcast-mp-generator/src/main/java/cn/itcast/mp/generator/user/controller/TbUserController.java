8e669d1c7decb1c19bf393f2af83b334ed24047e
Zlt <1627371580@qq.com>
2020-04-12 21:45:05 +0800

    数据库表生成实体类

---
+++ itcast-mp-generator/src/main/java/cn/itcast/mp/generator/user/controller/TbUserController.java
@@ -0,0 +1,20 @@
+package cn.itcast.mp.generator.user.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author itcast
+ * @since 2020-04-11
+ */
+@Controller
+@RequestMapping("/user/tb-user")
+public class TbUserController {
+
+}
---
+++ itcast-mp-generator/src/main/java/cn/itcast/mp/generator/user/entity/TbUser.java
@@ -0,0 +1,49 @@
+package cn.itcast.mp.generator.user.entity;
+
+    import java.io.Serializable;
+    import lombok.Data;
+    import lombok.EqualsAndHashCode;
+    import lombok.experimental.Accessors;
+
+/**
+* <p>
+    * 
+    * </p>
+*
+* @author itcast
+* @since 2020-04-11
+*/
+    @Data
+        @EqualsAndHashCode(callSuper = false)
+    @Accessors(chain = true)
+    public class TbUser implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+            /**
+            * 用户名
+            */
+    private String userName;
+
+            /**
+            * 密码
+            */
+    private String password;
+
+            /**
+            * 姓名
+            */
+    private String name;
+
+            /**
+            * 年龄
+            */
+    private Integer age;
+
+            /**
+            * 邮箱
+            */
+    private String email;
+
+
+}
